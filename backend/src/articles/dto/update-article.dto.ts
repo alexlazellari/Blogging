a74597afd658433fcd0d9bb99c3f1b2c35f062a0
Alex Lazellari <alex.lazellari12@gmail.com>
2024-05-18 16:01:32 +0100

    Initial setup

---
+++ backend/src/articles/dto/update-article.dto.ts
@@ -0,0 +1,7 @@
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateArticleDto } from './create-article.dto';
+
+export class UpdateArticleDto extends PartialType(CreateArticleDto) {
+  title: string;
+  content: string;
+}
---
+++ backend/src/articles/entities/article.entity.ts
@@ -0,0 +1,16 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Article {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column()
+  content: string;
+
+  @Column({ default: () => 'CURRENT_TIMESTAMP' })
+  created: Date;
+}
