40a97bf88cac4c934d6ced05c9b031ab3c565589
Alex Lazellari <alex.lazellari12@gmail.com>
2024-05-21 23:41:39 +0100

    feat(auth): setup JWT authentication with secure cookies

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,17 +5,24 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ArticlesModule } from './articles/articles.module';
 import { Article } from './articles/entities/article.entity';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { User } from './users/entities/user.entity';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     TypeOrmModule.forRoot({
       type: 'sqlite',
       database: 'db.sqlite',
-      entities: [Article],
+      entities: [Article, User],
       synchronize: true,
     }),
     ArticlesModule,
+    AuthModule,
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
